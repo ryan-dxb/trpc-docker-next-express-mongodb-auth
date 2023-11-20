84afc8f92b1f53236b7d3667c9a524948c5aed2f
Ryan Pereira <ryan.dxb92@gmail.com>
2023-11-20 11:29:46 +0400

    Completed Refresh Token Rotation logic for backend. Working on frontend

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -5,7 +5,11 @@ import { Button } from "ui";
 interface HomeProps {}
 
 const Home: NextPage<HomeProps> = () => {
-  return <div className="w-full h-screen"></div>;
+  return (
+    <div className="w-full h-screen">
+      <Button>Click me</Button>
+    </div>
+  );
 };
 
 export default Home;
